7ee2e8d5d296205770c3f444d2404e13b306f5e4
François Prigent <francois@prigent.xyz>
2021-01-22 14:49:59 +0100

    add test on duplicated company files, sync error variables

--- src/registerNewCompanyFile.test.js
+++ src/registerNewCompanyFile.test.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const registerNewCompanyFile = require('./registerNewCompanyFile');
 
 afterAll(() => {
-  fs.readdir(`${__dirname}/companyFiles`, (error, files) => {
+  fs.readdir(`${__dirname}/companyFiles`, (readDirectoryError, files) => {
     files.forEach((file) => {
-      fs.unlink(`${__dirname}/companyFiles/${file}`, (err) => {
-        if (err) { throw err; }
+      fs.unlink(`${__dirname}/companyFiles/${file}`, (deleteFileError) => {
+        if (deleteFileError) { throw deleteFileError; }
       });
     });
   });
 });
 
 test('Register a new company file without error', (done) => {
-  function callback(err) {
-    expect(err).toBe(null);
+  function callback(error) {
+    expect(error).toBe(null);
     done();
   }
 
@@ -26,6 +26,15 @@ test('Register a new company file with good content', (done) => {
     expect(companyFileContentJSON).toHaveProperty('companyName', 'NeXT');
     expect(companyFileContentJSON).toHaveProperty('companyFileCreationDate');
     expect(companyFileContentJSON).toHaveProperty('workers', []);
+    done();
+  }
+
+  registerNewCompanyFile('NeXT', callback);
+});
+
+test('Register a company which already exists', (done) => {
+  function callback(error) {
+    expect(error).toBe('File already exists');
     done();
   }
 
--- src/utils/fileOperations.test.js
+++ src/utils/fileOperations.test.js
@@ -1,30 +1,30 @@
 const { createFile, writeFile } = require('./fileOperations');
 
 test('Create a file', (done) => {
-  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err) => {
-    expect(err).toBe(null);
+  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (error) => {
+    expect(error).toBe(null);
     done();
   });
 });
 
 test('Create a file with a wrong path', (done) => {
-  createFile(`${__dirname}/companyFiles/testCreateFile.json`, (err) => {
-    expect(err).toBe('Failed to create file');
+  createFile(`${__dirname}/companyFiles/testCreateFile.json`, (error) => {
+    expect(error).toBe('Failed to create file');
     done();
   });
 });
 
 test('Create a file which already exists', (done) => {
-  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err) => {
-    expect(err).toBe('File already exists');
+  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (error) => {
+    expect(error).toBe('File already exists');
     done();
   });
 });
 
 test('Write in a file', (done) => {
-  createFile(`${__dirname}/../companyFiles/testWriteFile.json`, (err, fileDescriptor) => {
-    writeFile(fileDescriptor, 'content', (error, content) => {
-      expect(error).toBe(null);
+  createFile(`${__dirname}/../companyFiles/testWriteFile.json`, (createFileError, fileDescriptor) => {
+    writeFile(fileDescriptor, 'content', (writeFileError, content) => {
+      expect(writeFileError).toBe(null);
       expect(content).toBe('content');
       done();
     });
@@ -32,8 +32,8 @@ test('Write in a file', (done) => {
 });
 
 test('Write in a file with a wrong file descriptor', (done) => {
-  writeFile(11111, 'content', (err) => {
-    expect(err).toBe('Failed to edit file content');
+  writeFile(11111, 'content', (error) => {
+    expect(error).toBe('Failed to edit file content');
     done();
   });
 });
