819d34da4e429a29e724051da666d0f36fcb50b0
François Prigent <francois@prigent.xyz>
2021-01-22 14:31:37 +0100

    add and apply airbnb linter

--- src/registerNewCompanyFile.test.js
+++ src/registerNewCompanyFile.test.js
@@ -1,33 +1,33 @@
-const registerNewCompanyFile = require('./registerNewCompanyFile');
 const fs = require('fs');
+const registerNewCompanyFile = require('./registerNewCompanyFile');
 
 afterAll(() => {
-    fs.opendir(`${__dirname}/companyFiles`, async (error, dir) => {
-        for await (dirent of dir) {
-            fs.unlink(`${__dirname}/companyFiles/${dirent.name}`, (error) => {
-                if (error) { throw error; }
-            });
-        };
+  fs.readdir(`${__dirname}/companyFiles`, (error, files) => {
+    files.forEach((file) => {
+      fs.unlink(`${__dirname}/companyFiles/${file}`, (err) => {
+        if (err) { throw err; }
+      });
     });
+  });
 });
 
-test('Register a new company file without error', done => {
-    function callback(err, companyFileContent) {
-        expect(err).toBe(null);
-        done();
-    };
+test('Register a new company file without error', (done) => {
+  function callback(err) {
+    expect(err).toBe(null);
+    done();
+  }
 
-    registerNewCompanyFile('Pixar', callback);
+  registerNewCompanyFile('Pixar', callback);
 });
 
-test('Register a new company file with good content', done => {
-    function callback(err, companyFileContent) {
-        const companyFileContentJSON = JSON.parse(companyFileContent);
-        expect(companyFileContentJSON).toHaveProperty('companyName', 'NeXT');
-        expect(companyFileContentJSON).toHaveProperty('companyFileCreationDate');
-        expect(companyFileContentJSON).toHaveProperty('workers', []);
-        done();
-    }
+test('Register a new company file with good content', (done) => {
+  function callback(err, companyFileContent) {
+    const companyFileContentJSON = JSON.parse(companyFileContent);
+    expect(companyFileContentJSON).toHaveProperty('companyName', 'NeXT');
+    expect(companyFileContentJSON).toHaveProperty('companyFileCreationDate');
+    expect(companyFileContentJSON).toHaveProperty('workers', []);
+    done();
+  }
 
-    registerNewCompanyFile('NeXT', callback);
+  registerNewCompanyFile('NeXT', callback);
 });
--- src/utils/fileOperations.test.js
+++ src/utils/fileOperations.test.js
@@ -1,39 +1,39 @@
 const { createFile, writeFile } = require('./fileOperations');
 
-test('Create a file', done => {
-    createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err, fileDescriptior) => {
-        expect(err).toBe(null);
-        done();
-    })
+test('Create a file', (done) => {
+  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err) => {
+    expect(err).toBe(null);
+    done();
+  });
 });
 
-test('Create a file with a wrong path', done => {
-    createFile(`${__dirname}/companyFiles/testCreateFile.json`, (err, fileDescriptior) => {
-        expect(err).toBe('Failed to create file');
-        done();
+test('Create a file with a wrong path', (done) => {
+  createFile(`${__dirname}/companyFiles/testCreateFile.json`, (err) => {
+    expect(err).toBe('Failed to create file');
+    done();
+  });
+});
+
+test('Create a file which already exists', (done) => {
+  createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err) => {
+    expect(err).toBe('File already exists');
+    done();
+  });
+});
+
+test('Write in a file', (done) => {
+  createFile(`${__dirname}/../companyFiles/testWriteFile.json`, (err, fileDescriptor) => {
+    writeFile(fileDescriptor, 'content', (error, content) => {
+      expect(error).toBe(null);
+      expect(content).toBe('content');
+      done();
     });
+  });
 });
 
-test('Create a file which already exists', done => {
-    createFile(`${__dirname}/../companyFiles/testCreateFile.json`, (err, fileDescriptior) => {
-        expect(err).toBe('File already exists');
-        done();
-    })
-});
-
-test('Write in a file', done => {
-    createFile(`${__dirname}/../companyFiles/testWriteFile.json`, (err, fileDescriptor) => {
-        writeFile(fileDescriptor, 'content', function(err, content) {
-            expect(err).toBe(null);
-            expect(content).toBe('content');
-            done();
-        });
-    })
-});
-
-test('Write in a file with a wrong file descriptor', done => {
-    writeFile(11111, 'content', function(err, content) {
-        expect(err).toBe('Failed to edit file content');
-        done();
-    });
+test('Write in a file with a wrong file descriptor', (done) => {
+  writeFile(11111, 'content', (err) => {
+    expect(err).toBe('Failed to edit file content');
+    done();
+  });
 });
