5cdc34752ef7f823d30e3e83832fe7114530bd56
François Prigent <francois@prigent.xyz>
2021-01-26 14:43:58 +0100

    remove past js files

--- index.js
+++
@@ -1,9 +0,0 @@
-const registerNewCompanyFile = require('./src/registerNewCompanyFile');
-
-registerNewCompanyFile(process.argv[2], (error, companyFileContent) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-    return;
-  }
-  console.log(`Success: ${companyFileContent}`);
-});
